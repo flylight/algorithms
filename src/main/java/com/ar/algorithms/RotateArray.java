58bf6c4b4077c829cd6eeef2e5f4928e81ca5b9b
Andriy Rymar <andriyrav@gmail.com>
2017-01-13 11:18:16 +0200

    First commit

---
+++ src/main/java/com/ar/algorithms/RotateArray.java
@@ -0,0 +1,18 @@
+package com.ar.algorithms;
+
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+import javax.xml.transform.stream.StreamSource;
+
+/**
+ * Hello world!
+ *
+ */
+public class RotateArray
+{
+  public static void main( String[] args )
+  {
+    System.out.println( "Hello World!" );
+  }
+}
